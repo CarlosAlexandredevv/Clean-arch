c3770d85baa0e1e6f3f65a60596590b02bd66822
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-31 14:33:06 -0300

    :sparkles: feat: add controller registrarUsuario

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import RegistrarUsuarioController from "./controllers/RegistrarUsuarioController";
+import RegistrarUsuario from "./core/usuario/RegistrarUsuario";
+import ColecaoUsuarioDB from "./adapters/db/knex/ColecaoUsuarioDB";
+import CriptoReal from "./adapters/auth/CriptoReal";
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 const porta = process.env.PORT ?? 3001
 app.listen(porta, () =>{
     console.log(`Server is running on port ${porta}`)
 })
+
+const colecaoUsuario = new ColecaoUsuarioDB()
+const provedorCripto = new CriptoReal()
+const registrarUsuario = new RegistrarUsuario(colecaoUsuario, provedorCripto)
+new RegistrarUsuarioController(app, registrarUsuario)
+
